0b3ff2d6c171d8faeb5dad1c3dff88649273fddc
Jlevesque07 <joshual251@deltalearns.ca>
2025-02-14 22:43:40 +0000

    Started working on temperature converter.

---
+++ temp_coverter.py
@@ -0,0 +1,12 @@
+# TASK: Write a function that converts between Celsius and Fahrenheit.
+
+def temp_converter(unit: str) -> int:
+    """Function takes input of celcius or farenheight, and outputs the opposite."""
+    if unit == c:
+        temp1 = int(input("What is your temperature in degrees Celcius? "))
+
+
+
+temp_converter(input("Do you want to convert a temperature in degrees Celcius or farenheight? [c/f]").strip().lower())
+
+print(temp2)
